18ce33794c164e5654f77e367f9d04e1e2434995
Ragnar Olafsson <ragnar.orn@gmail.com>
2024-07-27 22:57:10 +0200

    add copilot to nvim config

---
+++ lua/roo/plugins/copilot.lua
@@ -0,0 +1,45 @@
+return {
+	"zbirenbaum/copilot.lua",
+	cmd = "Copilot",
+	event = "InsertEnter",
+	config = function()
+		require('copilot').setup({
+			panel = {
+				enabled = true,
+				auto_refresh = false,
+				keymap = {
+					jump_prev = "[[",
+					jump_next = "]]",
+					accept = "<CR>",
+					refresh = "gr",
+					open = "<M-CR>"
+				},
+				layout = {
+					position = "bottom", -- | top | left | right
+					ratio = 0.4
+				},
+			},
+			suggestion = {
+				enabled = true,
+				auto_trigger = true,
+				hide_during_completion = true,
+				debounce = 75,
+				keymap = {
+					accept = "<Right>",
+				},
+			},
+			filetypes = {
+				yaml = false,
+				markdown = false,
+				help = false,
+				gitcommit = false,
+				gitrebase = false,
+				hgcommit = false,
+				svn = false,
+				cvs = false,
+				["."] = false,
+			},
+			server_opts_overrides = {},
+		})
+	end,
+}
