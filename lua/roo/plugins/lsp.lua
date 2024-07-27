b81223085c77967824807fd8c2f4bd9ae7d6437c
Ragnar Olafsson <ragnar.orn@gmail.com>
2024-07-27 21:51:44 +0200

    add mason for LSP support

---
+++ lua/roo/plugins/lsp.lua
@@ -0,0 +1,11 @@
+return {
+    "williamboman/mason-lspconfig.nvim",
+  dependencies = {
+    "williamboman/mason.nvim",
+    "neovim/nvim-lspconfig",
+  },
+  config = function()
+    require("mason").setup()
+    require("mason-lspconfig").setup()
+  end
+}
