c50a19e5bc5976f49f21baf5a782755711c6cf2d
Ragnar Olafsson <ragnar.orn@gmail.com>
2024-07-29 14:20:48 +0200

    add keymap for sourcing

--- lua/roo/core/keymaps.lua
+++ lua/roo/core/keymaps.lua
@@ -2,3 +2,8 @@ vim.g.mapleader = ","
 vim.g.maplocalleader = "\\"
 
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>" )
+vim.keymap.set("n", "<leader>ee", "<cmd>Explore<cr>")
+
+vim.keymap.set("n", "<leader><leader>", function()
+    vim.cmd("so")
+end)
