bb7bcc82c1b051d8120601e9e7bfbb2dcc7e0985
Ragnar Olafsson <ragnar.orn@gmail.com>
2024-07-30 19:32:55 +0200

    add dependencies for cmp

--- lua/roo/plugins/cmp.lua
+++ lua/roo/plugins/cmp.lua
@@ -1,5 +1,11 @@
 return {
 	"hrsh7th/nvim-cmp",
+	dependencies = {
+		'hrsh7th/cmp-nvim-lsp',
+		'hrsh7th/cmp-buffer',
+		'hrsh7th/cmp-path',
+		'hrsh7th/cmp-cmdline',
+	},
 	setup = function()
 		require('cmp').setup()
 	end,
