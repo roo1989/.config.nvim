0b8f0bed60acd06183a68783e93ff33953f1b7b9
Ragnar Olafsson <ragnar.orn@gmail.com>
2024-07-31 20:10:12 +0200

    finally get cmp to work

--- lua/roo/plugins/cmp.lua
+++ lua/roo/plugins/cmp.lua
@@ -1,12 +1,34 @@
 return {
 	"hrsh7th/nvim-cmp",
+	event = 'InsertEnter',
 	dependencies = {
 		'hrsh7th/cmp-nvim-lsp',
-		'hrsh7th/cmp-buffer',
 		'hrsh7th/cmp-path',
-		'hrsh7th/cmp-cmdline',
 	},
-	setup = function()
-		require('cmp').setup()
+	config = function()
+		local cmp = require "cmp"
+
+		cmp.setup {
+			completion = { completeopt = 'menu,menuone,noinsert' },
+			mapping = {
+				["<Tab>"] = cmp.mapping.select_next_item(),
+				["<S-Tab>"] = cmp.mapping.select_prev_item(),
+				["<C-j>"] = cmp.mapping.select_next_item(),
+				["<C-k>"] = cmp.mapping.select_prev_item(),
+				["<C-d>"] = cmp.mapping.scroll_docs(-4),
+				["<C-f>"] = cmp.mapping.scroll_docs(4),
+				["<C-Space>"] = cmp.mapping.complete(),
+				["<C-e>"] = cmp.mapping.close(),
+				["<CR>"] = cmp.mapping.confirm {
+					behavior = cmp.ConfirmBehavior.Insert,
+					select = true,
+				},
+			},
+			sources = {
+				{ name = "nvim_lsp" },
+				{ name = "path" },
+			},
+		}
 	end,
 }
+
