ba29f57b953a2fe00c39f3e45ca26bf03477ff0c
Ragnar Olafsson <ragnar.orn@gmail.com>
2024-08-01 16:48:45 +0200

    change keymaps for neotree and toggleterm

--- lua/roo/core/keymaps.lua
+++ lua/roo/core/keymaps.lua
@@ -2,8 +2,12 @@ vim.g.mapleader = ","
 vim.g.maplocalleader = "\\"
 
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>" )
-vim.keymap.set("n", "<leader>ee", "<cmd>Explore<cr>")
+vim.keymap.set("n", "<leader>ee", "<cmd>Neotree<cr>")
 
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so ~/.config/nvim/init.lua")
+end)
+
+vim.keymap.set("n", "<leader>tt" , function()
+	vim.cmd("ToggleTerm direction=float dir=.")
 end)
