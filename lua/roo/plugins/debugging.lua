15f62165c3a244d88a32cf4b4fbb1dbddf3a6105
Ragnar Olafsson <ragnar.orn@gmail.com>
2024-08-01 16:06:59 +0200

    sane defaults for dap

--- lua/roo/plugins/debugging.lua
+++ lua/roo/plugins/debugging.lua
@@ -1,27 +1,56 @@
 return {
-	"mfussenegger/nvim-dap",
-	dependencies = {
-		"nvim-neotest/nvim-nio",
-		"rcarriga/nvim-dap-ui",
+	{
+		"mfussenegger/nvim-dap-python",
+		config = function()
+			local dap_python = require("dap-python")
+			dap_python.setup("/Users/emil/.pyenv/versions/nvim-dap/bin/python")
+			dap_python.test_runner = "pytest"
 
+			vim.keymap.set("n", "<leader>dn", dap_python.test_method, {})
+			vim.keymap.set("n", "<leader>df", dap_python.test_class, {})
+			vim.keymap.set("v", "<leader>ds", dap_python.debug_selection, {})
+		end,
 	},
-	config = function()
-		local dap = require "dap"
-		local dapui = require "dapui"
-		dap.listeners.before.attach.dapui_config = function()
-			dapui.open()
-		end
-		dap.listeners.before.launch.dapui_config = function()
-			dapui.open()
-		end
-		dap.listeners.before.event_terminated.dapui_config = function()
-			dapui.close()
-		end
-		dap.listeners.before.event_exited.dapui_config = function()
-			dapui.close()
-		end
+	{
+		"mfussenegger/nvim-dap",
+		dependencies = { "rcarriga/nvim-dap-ui", "nvim-neotest/nvim-nio" },
+		config = function()
+			local dap, dapui = require("dap"), require("dapui")
+			dapui.setup()
+			dap.listeners.before.attach.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.launch.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.event_terminated.dapui_config = function()
+				dapui.close()
+			end
+			dap.listeners.before.event_exited.dapui_config = function()
+				dapui.close()
+			end
 
-		vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, {})
-		vim.keymap.set("n", "<leader>dc", dap.continue, {})
-	end
+			dap.configurations.python = {
+				{
+					name = "Python: Django ELKO",
+					type = "python",
+					request = "launch",
+					program = "${workspaceFolder}/src/python/elko/manage.py",
+					args = {
+						"runserver",
+					},
+					django = true,
+					justMyCode = true,
+					purpose = {
+						"debug-test",
+					},
+				},
+			}
+			vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint, {})
+			vim.keymap.set("n", "<F5>", dap.continue, {})
+			vim.keymap.set("n", "<F10>", dap.step_over, {})
+			vim.keymap.set("n", "<F11>", dap.step_into, {})
+			vim.keymap.set("n", "<F12>", dap.step_out, {})
+		end,
+	},
 }
