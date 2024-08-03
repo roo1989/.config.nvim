e801bd13522653bfaebc7cd5d9420a4c4538149d
Ragnar Olafsson <ragnar.orn@gmail.com>
2024-08-03 20:59:59 +0200

    fix keymap for neotree

--- lua/roo/core/keymaps.lua
+++ lua/roo/core/keymaps.lua
@@ -2,7 +2,7 @@ vim.g.mapleader = ","
 vim.g.maplocalleader = "\\"
 
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>" )
-vim.keymap.set("n", "<leader>ee", "<cmd>Neotree<cr>")
+vim.keymap.set("n", "<leader>ee", "<cmd>Neotree toggle<cr>")
 
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so ~/.config/nvim/init.lua")
